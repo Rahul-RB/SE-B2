450f31e1931e9083e9dd88a13c6b2838c8332e70
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-10-07 18:05:31 +0530

    Added login page to templates/ ; static/ and views.py

--- hawkeye/hawkeye/views.py
+++ hawkeye/hawkeye/views.py
@@ -1,5 +1,10 @@
 from hawkeye import app
 from flask import render_template
+
 @app.route("/")
-def hello():
-	return render_template("Home/home.html",user="User",title="Home")
+def home():
+	return render_template("Home/home.html",title="User")
+
+@app.route("/login")
+def login():
+	return render_template("Login/login.html")
