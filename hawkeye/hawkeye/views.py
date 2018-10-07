76e96c0682a06abb24ed582d5bbb247040740d94
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-10-07 19:05:51 +0530

    Added Register files to templates/ ; static/ and views.py

--- hawkeye/hawkeye/views.py
+++ hawkeye/hawkeye/views.py
@@ -8,3 +8,19 @@ def home():
 @app.route("/login")
 def login():
 	return render_template("Login/login.html")
+
+@app.route("/register_patient")
+def register_patient():
+	return render_template("Register/register_patient.html",title="Patient")
+
+@app.route("/register_doctor")
+def register_doctor():
+	return render_template("Register/register_doctor.html",title="Doctor")
+
+@app.route("/register_lab")
+def register_lab():
+	return render_template("Register/register_lab.html",title="Lab")
+
+@app.route("/register_pharmacy")
+def register_pharmacy():
+	return render_template("Register/register_pharmacy.html",title="Pharmacy")
