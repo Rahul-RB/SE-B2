0920a931ab232d325f5676798a3b65b23210cbf8
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-10-11 00:13:26 +0530

    Added basic code to forms.py

--- hawkeye/hawkeye/forms.py
+++ hawkeye/hawkeye/forms.py
@@ -1,1 +1,6 @@
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+class SignupForm(FlaskForm):
+    email = StringField('email')
+    password = PasswordField('password')
+    submit = SubmitField("Sign In")
