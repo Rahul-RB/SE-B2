a4d7810e4a68b5aaae53b9e091f5d12877d026c9
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-10-11 14:46:42 +0530

    Removed MySQL password from __init__.py

--- hawkeye/hawkeye/__init__.py
+++ hawkeye/hawkeye/__init__.py
@@ -7,7 +7,7 @@ app.config["TEMPLATES_AUTO_RELOAD"]=True
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'RahulRB@1997'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''# Put your MySQL root password here
 app.config['MYSQL_DATABASE_DB'] = 'Hawkeye'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
