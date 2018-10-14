ea6b1566447aa72d138ec6dd458ed3ea065a9e06
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-10-14 17:51:49 +0530

    Moved configs from __init__ to config.py | Added fontawesome web-fonts | Updated requirements.txt

--- hawkeye/config.py
+++ hawkeye/config.py
@@ -1,1 +1,21 @@
+class Config(object):
+    DEBUG = True
+    TESTING = False
 
+class ProductionConfig(Config):
+    DEBUG = False
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''# Put your MySQL root password here
+    MYSQL_DATABASE_DB = 'Hawkeye'
+    MYSQL_DATABASE_HOST = 'localhost'
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TEMPLATES_AUTO_RELOAD=True
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = ''# Put your MySQL root password here
+    MYSQL_DATABASE_DB = 'Hawkeye'
+    MYSQL_DATABASE_HOST = 'localhost'
+
+class TestingConfig(Config):
+    TESTING = True
