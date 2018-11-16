34a4a4187c97b94d6e405b0016bbe0fead4bc9a2
DJPooja <poojadurgoji@gmail.com>
2018-11-16 16:51:42 +0530

    Fixed Inconsistent use of tabs

--- hawkeye/hawkeye/views/views_pharmacy.py
+++ hawkeye/hawkeye/views/views_pharmacy.py
@@ -16,27 +16,24 @@ import datetime
 app.secret_key = 'secretkeyhereplease'
 
 #pharmacy
-@app.route("/pharmacy")
+app.route("/pharmacy")
 def pharmacy():
-    if ((not session.get("accType")=="Pharmacy") or (not session.get(session.get("accType")+"LoggedIn"))):
-        return redirect(url_for("login"),302)
-    email=session["currentEmail"]
-    #session["user_id"] = models.getpharmacyID(email)[0][0];
-    #print(session["user_id"])
-    #print("-----------------",models.getpharmacytitle(email)[0])
-    return render_template("Pharmacy/pharmacy.html",title="Pharmacy", data=models.getpharmacytitle(email))
-	
-	
+	if ((not session.get("accType")=="Pharmacy") or (not session.get(session.get("accType")+"LoggedIn"))):
+		return redirect(url_for("login"),302)
+	email=session["currentEmail"]
+	#session["user_id"] = models.getpharmacyID(email)[0][0];
+	#print(session["user_id"])
+	#print("-----------------",models.getpharmacytitle(email)[0])
+	return render_template("Pharmacy/pharmacy.html",title="Pharmacy", data=models.getpharmacytitle(email),userLoggedIn=True)
+
 #pharmacy_prescription
 @app.route("/pharmacy_prescription",methods=["POST"])
 def pharmacy_prescription():
-    if ((not session.get("accType")=="Pharmacy") or (not session.get(session.get("accType")+"LoggedIn"))):
-        return redirect(url_for("login"),302)
-    email=session["currentEmail"]
-    patid=request.form.get('patID')
-    #email=session["currentEmail"]
-    #print(patid)
-    print(models.getpharmacyPres(email,patid), models.getEprescitionDetails(email,patid))
-	return render_template("Pharmacy/pharmacy_prescription.html",title="Pharmacy",data=models.getpharmacytitle(email), pharPresData=models.getpharmacyPres(email,patid),details= models.getEprescitionDetails(email,patid),userLoggedIn=True)
-	
-#end function
+	if ((not session.get("accType")=="Pharmacy") or (not session.get(session.get("accType")+"LoggedIn"))):
+        	return redirect(url_for("login"),302)
+	email=session["currentEmail"]
+	patid=request.form.get('patID')
+	#email=session["currentEmail"]
+	#print(patid)
+	#print(models.getpharmacyPres(email,patid), models.getEprescitionDetails(email,patid))
+	return render_template("Pharmacy/pharmacy_prescription.html",title="Pharmacy",data=models.getpharmacytitle(email), pharPresData=models.getpharmacyPres(email,patid),details= models.getEprescriptionDetails(email,patid),userLoggedIn=True)
