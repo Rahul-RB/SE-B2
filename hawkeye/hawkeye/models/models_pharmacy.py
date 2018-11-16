0bf39758917fa13066e2d6e2ae82968a53503bad
DJPooja <poojadurgoji@gmail.com>
2018-11-16 15:23:35 +0530

    Uploading required files for pharmacy

--- hawkeye/hawkeye/models/models_pharmacy.py
+++ hawkeye/hawkeye/models/models_pharmacy.py
@@ -1,7 +1,43 @@
-from hawkeye import mysql
-import json
-import datetime
-import time  
-import numpy as np
-import matplotlib.pyplot as plt  
+# START : Pharmacy FUNCTIONS
+# Get the required details for the pharmacy
+def getpharmacytitle(email):
+    query="SELECT p.pharmacyName, p.address, p.phoneNO from PharmacyDetails p, PharmacyLogin  WHERE PharmacyLogin.email='"+email+"';"
+    print(query)
+    conn = mysql.connect()
+    cursor =mysql.get_db().cursor()
+    res = cursor.execute(query)
+    data = cursor.fetchall()
+    #print("Hi in sql ###############",data)
+    cursor.close()
+    conn.close()
+    return (data)
 
+
+	
+# Fetch the EPrescription for particular patient
+def getpharmacyPres(email,patientID):
+    query="SELECT p.slNo, p.medicineSuggestion, p.remarks from EPrescription p where p.patientID='"+patientID+"';"
+    print(query)
+    conn = mysql.connect()
+    cursor =mysql.get_db().cursor()
+    res = cursor.execute(query)
+    data = cursor.fetchall()
+    #print("Hi in sql ###############",data)
+    cursor.close()
+    conn.close()
+    return (data)
+
+	
+	
+# fetch other details from EPrescription
+def getEprescitionDetails(email,patientID):
+    query="SELECT p.patientID, p.ePrescriptionID, p.doctorID from EPrescription p WHERE p.patientID='"+patientID+"'; "
+    print(query)
+    conn = mysql.connect()
+    cursor =mysql.get_db().cursor()
+    res = cursor.execute(query)
+    data = cursor.fetchall()
+    #print("Hi in sql ###############",data)
+    cursor.close()
+    conn.close()
+    return (data)
