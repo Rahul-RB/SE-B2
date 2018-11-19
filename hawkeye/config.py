d4414943f7e681fd36129560107ca1216cb3b732
Gaurav2609 <peswani.grv9@gmail.com>
2018-11-19 12:52:01 +0530

    Improved patient history UI

--- hawkeye/config.py
+++ hawkeye/config.py
@@ -13,8 +13,8 @@ class DevelopmentConfig(Config):
     DEBUG = True
     TEMPLATES_AUTO_RELOAD=True
     MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = 'RahulRB@1997'# Put your MySQL root password here
-    MYSQL_DATABASE_DB = 'HawkeyeWithData'
+    MYSQL_DATABASE_PASSWORD = ''# Put your MySQL root password here
+    MYSQL_DATABASE_DB = 'Hawkeye'
     MYSQL_DATABASE_HOST = 'localhost'
     CACHE_TYPE = "null"
     UPLOAD_FOLDER = './uploads'
